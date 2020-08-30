cae759b903ab99c35a1ff8e2ec8beb32a6acc4bc
cprobbie <cprobbie23@gmail.com>
2020-08-30 18:44:46 +1000

    added Delete Option processor

---
+++ src/RefactorThis.Core/OptionProcessor/DeleteOptionRequestProcessor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using RefactorThis.Core.Interfaces;
+
+namespace RefactorThis.Core.OptionProcessor
+{
+    public class DeleteOptionRequestProcessor
+    {
+        private readonly IProductRepository _productRepository;
+
+        public DeleteOptionRequestProcessor(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public void DeleteProductOption(Guid productId, Guid optionId)
+        {
+            var existingOption = _productRepository.GetOption(productId, optionId);
+            if (existingOption is null)
+            {
+                throw new ArgumentException("Product Option not found");
+            }
+            _productRepository.Delete(optionId);
+        }
+    }
+}
---
+++ tests/RefactorThis.Tests.Unit/OptionProcessor/DeleteOptionRequestProcessorTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using FluentAssertions;
+using Moq;
+
+using NUnit.Framework;
+
+using RefactorThis.Core.Domain;
+using RefactorThis.Core.Interfaces;
+using RefactorThis.Core.OptionProcessor;
+
+namespace RefactorThis.Core.Unit.OptionProcessor
+{
+    [TestFixture]
+    public class DeleteOptionRequestProcessorTests
+    {
+        private Mock<IProductRepository> _productRepositoryMock;
+        private DeleteOptionRequestProcessor _SUT;
+        private ProductOption _option;
+
+        [SetUp]
+        public void Setup()
+        {
+            _productRepositoryMock = new Mock<IProductRepository>();
+            _SUT = new DeleteOptionRequestProcessor(_productRepositoryMock.Object);
+            _option = new ProductOption(Guid.NewGuid(), "32G", "32G storage");
+        }
+
+        [Test]
+        public void GiveValidInputs_ShouldDeleteProductOption()
+        {
+            // Arrange
+            _productRepositoryMock.Setup(x => x.GetOption(It.IsAny<Guid>(), _option.Id)).Returns(_option);
+
+            // Act
+            _SUT.DeleteProductOption(It.IsAny<Guid>(), _option.Id);
+
+            // Assert
+            _productRepositoryMock.Verify(x => x.Delete(It.IsAny<Guid>()), Times.Once);
+        }
+
+        [Test]
+        public void GivenOptionIdNonExists_ShouldThrowArgumentException()
+        {
+            // Arrange
+            _productRepositoryMock.Setup(x => x.GetOption(It.IsAny<Guid>(), _option.Id)).Returns((ProductOption)null);
+
+            // Act and Assert
+            _SUT.Invoking(x => x.DeleteProductOption(It.IsAny<Guid>(), It.IsAny<Guid>()))
+                .Should().Throw<ArgumentException>().WithMessage("Product Option not found");
+        }
+    }
+}
