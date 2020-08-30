32b23ad5d90fe29359b1617edfd72a051d135bc8
cprobbie <cprobbie23@gmail.com>
2020-08-30 21:53:53 +1000

    added exception filter

---
+++ src/RefactorThis.Api/ExceptionFilter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace RefactorThis.Api
+{
+    public class ExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            var errorMessage = "Internal Server Error";
+            var code = HttpStatusCode.InternalServerError;
+            if (context.Exception is ArgumentException argEx)
+            {
+                code = HttpStatusCode.BadRequest;
+                errorMessage = argEx.Message;
+            }
+            else if (context.Exception is KeyNotFoundException notFoundEx)
+            {
+                code = HttpStatusCode.NotFound;
+                errorMessage = notFoundEx.Message;
+            }
+
+            var result = new ObjectResult(errorMessage) { StatusCode = (int)code};
+            context.Result = result;
+            context.HttpContext.Response.ContentType = "application/json";
+            context.HttpContext.Response.StatusCode = (int)code;
+        }
+    }
+}
--- src/RefactorThis.Api/Startup.cs
+++ src/RefactorThis.Api/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
+using RefactorThis.Api;
+
 namespace RefactorThis
 {
     public class Startup
@@ -26,7 +28,10 @@ namespace RefactorThis
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers(options => 
+            { 
+                options.Filters.Add(typeof(ExceptionFilter)); 
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
